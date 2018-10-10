5b71948edbda9ece7cb94f80f80d9b5fe4c595bf
Kitson Kelly <me@kitsonkelly.com>
2018-10-11 10:36:57 +1100

    More WIP

--- tools/ts_doc_generator/generate_json_docs.ts
+++ tools/ts_doc_generator/generate_json_docs.ts
@@ -1,7 +1,15 @@
 import {
+  FunctionDeclaration,
+  InterfaceDeclaration,
+  ModuleResolutionKind,
   NamespaceDeclaration,
   NamespaceDeclarationKind,
+  Node,
   Project,
+  ScriptTarget,
+  SyntaxKind,
+  SyntaxList,
+  Type,
   TypeGuards
 } from "ts-simple-ast";
 
@@ -16,13 +24,32 @@ interface GenerateJsonDocsOptions {
 
 interface DocumentationNode {
   type: string;
+  name: string;
+}
+
+interface ChildableNode extends DocumentationNode {
   children: DocumentationNode[];
 }
 
-interface NamespaceNode extends DocumentationNode {
+interface NamespaceNode extends ChildableNode {
   type: "namespace";
-  name: string;
   kind: "global" | "module" | "namespace";
+}
+
+interface FunctionNode extends DocumentationNode {
+  type: "function";
+  returnType: TypeNode;
+  docs?: string;
+}
+
+interface TypeNode {
+  text: string;
+  symbol?: string;
+  aliasSymbol?: string;
+}
+
+interface InterfaceNode extends ChildableNode {
+  type: "interface";
 }
 
 export function main({
@@ -33,6 +60,77 @@ export function main({
   silent
 }: GenerateJsonDocsOptions): void {
   const globalScopeChildren: DocumentationNode[] = [];
+
+  function parseSyntaxList(syntaxList: SyntaxList): DocumentationNode[] {
+    return mapChildren(syntaxList.getChildren());
+  }
+
+  function parseType(type: Type, enclosingNode?: Node): TypeNode {
+    const text = type.getText(enclosingNode);
+    const symbol = type.getSymbol();
+    const aliasSymbol = type.getAliasSymbol();
+    return {
+      text,
+      symbol: symbol && symbol.getFullyQualifiedName(),
+      aliasSymbol: aliasSymbol && aliasSymbol.getFullyQualifiedName()
+    };
+  }
+
+  function parseFunctionDeclaration(
+    functionDeclaration: FunctionDeclaration
+  ): FunctionNode {
+    const type = "function";
+    const name = functionDeclaration.getName() || "(anonymous)";
+    // const params = functionDeclaration.getParameters();
+    const returnType = parseType(
+      functionDeclaration.getReturnType(),
+      functionDeclaration
+    );
+    const docs =
+      functionDeclaration
+        .getJsDocs()
+        .map(jsdoc => jsdoc.getInnerText())
+        .join("\n") || undefined;
+    return {
+      type,
+      name,
+      returnType,
+      docs
+    };
+  }
+
+  function parseInterfaceDeclaration(
+    interfaceDeclaration: InterfaceDeclaration
+  ): InterfaceNode {
+    const type = "interface";
+    const name = interfaceDeclaration.getName();
+    interfaceDeclaration.getChildren().forEach(node => {
+      console.log(name, node.getKindName());
+    });
+    return {
+      type,
+      name,
+      children: []
+    };
+  }
+
+  function mapChildren(children: Node[]): DocumentationNode[] {
+    const output: DocumentationNode[] = [];
+    for (const node of children) {
+      if (TypeGuards.isNamespaceDeclaration(node)) {
+        output.push(parseNamespace(node));
+      } else if (TypeGuards.isSyntaxList(node)) {
+        output.push(...parseSyntaxList(node));
+      } else if (TypeGuards.isFunctionDeclaration(node)) {
+        output.push(parseFunctionDeclaration(node));
+      } else if (TypeGuards.isInterfaceDeclaration(node)) {
+        output.push(parseInterfaceDeclaration(node));
+      } else {
+        console.log(node.getKindName());
+      }
+    }
+    return output;
+  }
 
   function parseNamespace(namespace: NamespaceDeclaration): NamespaceNode {
     const type = "namespace";
@@ -45,9 +143,11 @@ export function main({
           ? "module"
           : "namespace";
 
-    const children = namespace.getChildren().map(node => {
-      if ()
-    });
+    const [moduleBock] = namespace.getChildrenOfKind(SyntaxKind.ModuleBlock);
+    if (!moduleBock) {
+      throw new Error(`Namespace "${name}" missing module block.`);
+    }
+    const children = mapChildren(moduleBock.getChildren());
 
     return {
       type,
@@ -59,8 +159,12 @@ export function main({
 
   const inputProject = new Project({
     compilerOptions: {
+      baseUrl: buildPath,
+      moduleResolution: ModuleResolutionKind.NodeJs,
       noLib: true,
-      strict: true
+      strict: true,
+      target: ScriptTarget.ESNext,
+      types: ["text-encoding"]
     }
   });
 
